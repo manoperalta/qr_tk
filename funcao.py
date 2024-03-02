3950ef3d07b60aecd7e8d370d807b72703d27eba
Jonathas Martinelli Peralta <63939605+manoperalta@users.noreply.github.com>
2024-03-02 15:59:29 -0300

    Update funcao.py

--- funcao.py
+++ funcao.py
@@ -4,7 +4,7 @@ def converter(nome, link):
     nome_qr = nome
     link_qr = link
     imagem_qrcoded = qrcode.make(link_qr)
-    path = f"qrcode_{nome_qr}.png"  # Corrigindo esta linha para obter o caminho do arquivo
+    path = f"qrcode_{nome_qr}.png"
     imagem_qrcoded.save(path)
     print(path)
     return path
